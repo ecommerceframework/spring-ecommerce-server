18daf56347ecf066ddeb7270679457f968f20f81
Fu Xie <fuxiedev@gmail.com>
2023-09-16 01:08:11 +0800

    feat: create new project

---
+++ src/main/java/dev/fuxie/springecommerceserver/SpringEcommerceServerApplication.java
@@ -0,0 +1,13 @@
+package dev.fuxie.springecommerceserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringEcommerceServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringEcommerceServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/fuxie/springecommerceserver/SpringEcommerceServerApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.fuxie.springecommerceserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringEcommerceServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
